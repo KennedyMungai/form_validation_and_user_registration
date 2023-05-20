8c0cce2a2676ce60ad17158c22f9294a76e7d5b7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-20 12:06:46 +0300

    Created two regex components

--- src/components/register.tsx
+++ src/components/register.tsx
@@ -5,6 +5,9 @@ import { faCheck, faTimes, faInfoCircle } from 'react-icons/fa'
 type Props = {}
 
 const Register = (props: Props) => {
+	const userRegex = ''
+	const passRegex = ''
+
 	return <div>Register</div>
 }
 
