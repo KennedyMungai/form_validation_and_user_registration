e3efe09df30891c15246a4ceb9b6de26d298b770
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-20 12:10:53 +0300

    Created two null refs

--- src/components/register.tsx
+++ src/components/register.tsx
@@ -7,6 +7,9 @@ const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
 
 const Register = (props: Props) => {
+	const userRef = useRef<HTMLInputElement>(null)
+	const errRef = useRef<HTMLInputElement>(null)
+
 	return <div>Register</div>
 }
 
