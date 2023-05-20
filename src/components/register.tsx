efa79a9d25501a4ee3537148b05d09350d7ef21c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-20 12:08:57 +0300

    Added the regex templates for the user and the password

--- src/components/register.tsx
+++ src/components/register.tsx
@@ -3,11 +3,10 @@ import { useEffect, useRef, useState } from 'react'
 import { FaCheck, FaTimes, FaInfoCircle } from 'react-icons/fa'
 
 type Props = {}
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
 
 const Register = (props: Props) => {
-	const userRegex = ''
-	const passRegex = ''
-
 	return <div>Register</div>
 }
 
