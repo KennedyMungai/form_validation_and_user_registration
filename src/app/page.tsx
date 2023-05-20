aa5ef54393a553b5b5a0647b6c76d3f6da492e0e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-20 11:57:38 +0300

    Fixed a small issue with the styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
 	return (
-		<main className='flex justify-center align-items h-screen w-screen'></main>
+		<main className='flex justify-center items-center h-screen w-screen'>
+			<h1>Hello</h1>
+		</main>
 	)
 }
