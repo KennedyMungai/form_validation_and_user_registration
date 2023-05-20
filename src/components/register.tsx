9782ce9343aca20b850aa1b12dfbe5c75f85d31f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-20 12:05:44 +0300

    Updated a few spellings

--- src/components/register.tsx
+++ src/components/register.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useEffect, useRef, useState } from 'react'
-import { FaCheck, faTimes, faInfoCircle } from 'react-icons/fa'
+import { faCheck, faTimes, faInfoCircle } from 'react-icons/fa'
 
 type Props = {}
 
